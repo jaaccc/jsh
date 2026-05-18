d8d1bc51f2cd43ff4d431d7279da1d894f5ac7a0
jaaccc <djacobmohamed@gmail.com>
2026-05-18 10:09:11 -0400

    implemented read line function

--- jsh.c
+++ jsh.c
@@ -1,4 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define READ_BUFFER_SIZE 256
+
+char *jsh_read(void) {
+    int buffer_size = READ_BUFFER_SIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * buffer_size);
+    int character;
+
+    if (!buffer) {
+        fprintf(stderr, "jsh: allocation error\n");
+        exit(1);
+    }
+
+    while (1) {
+        character = getchar();
+
+        if (character == EOF || character == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = character;
+        }
+        position++;
+
+        if (position >= buffer_size) {
+            buffer_size += READ_BUFFER_SIZE;
+            buffer = realloc(buffer, buffer_size);
+
+            if (!buffer) {
+                fprintf(stderr, "jsh: allocation error\n");
+                exit(1);
+            }
+        }
+    }
+}
 
 void jsh_loop(void) {
     char *line;
